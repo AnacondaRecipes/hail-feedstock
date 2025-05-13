e1cad09892fccf3b214dd2c86e7d170f349fefd1
ViridianMelody <mlove@anaconda.com>
2025-05-13 13:25:20 -0600

    Install wheel built by make install

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,4 +4,5 @@
 # https://hail.is/docs/0.2/getting_started_developing.html#requirements
 pushd $SRC_DIR/hail
 make install HAIL_COMPILE_NATIVES=1 -j ${CPU_COUNT}
+$PYTHON -m pip install build/deploy/dist/hail-0.2.134-py3-none-any.whl --no-deps --no-build-isolation
 popd
